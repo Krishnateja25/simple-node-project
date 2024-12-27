df38cb9e172dd6997a2584c0431c4207f4386e28
Krishnateja25 <pisupatikrishnateja616@gmail.com>
2024-12-27 11:51:00 +0530

    Introduce bug in add function and test for failure

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-// app.js
+// // app.js
+// function add(a, b) {
+//     return a + b;
+//   }
+  
+//   module.exports = add;
+
+  // app.js
 function add(a, b) {
-    return a + b;
+    // Changing logic: return incorrect sum
+    return a + b + 1;  // Intentional bug: adding 1 to the sum
   }
   
   module.exports = add;
+  
   
--- test.js
+++ test.js
@@ -1,6 +1,14 @@
+// // test.js
+// const add = require('./app');
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(add(1, 2)).toBe(3);
+// });
+
+
 // test.js
 const add = require('./app');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+test('adds 1 + 2 to equal 4', () => {  // Incorrect expected result
+  expect(add(1, 2)).toBe(4);  // This will fail because 1 + 2 + 1 = 4
 });
