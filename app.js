87f8fcdd32c4e5bac62ab5390ab11b910087f8bc
Krishnateja25 <pisupatikrishnateja616@gmail.com>
2024-12-27 12:01:51 +0530

    Ensure test fails by introducing bug in add function

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-// // app.js
-// function add(a, b) {
-//     return a + b;
-//   }
-  
-//   module.exports = add;
-
-  // app.js
+// app.js
 function add(a, b) {
-    // Changing logic: return incorrect sum
-    return a + b + 1;  // Intentional bug: adding 1 to the sum
+    return a + b;  // Intentional bug: adds 1 to the result of a + b
   }
   
   module.exports = add;
-  
   
--- test.js
+++ test.js
@@ -1,14 +1,6 @@
-// // test.js
-// const add = require('./app');
-
-// test('adds 1 + 2 to equal 3', () => {
-//   expect(add(1, 2)).toBe(3);
-// });
-
-
 // test.js
 const add = require('./app');
 
-test('adds 1 + 2 to equal 4', () => {  // Incorrect expected result
-  expect(add(1, 2)).toBe(4);  // This will fail because 1 + 2 + 1 = 4
+test('adds 1 + 2 to equal 100', () => {  // Force a failure by expecting an impossible result
+  expect(add(1, 2)).toBe(100);  // This will fail because the sum is 3, not 100
 });
